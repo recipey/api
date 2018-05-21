083d2acf079da10788652cc6c8c8d3c4296becae
Jon Ho <jonwho.contact@gmail.com>
2018-05-20 21:54:34 -0700

    create recipe and recipe_steps tables (fixes #7)

--- migrations/1526789624_create_recipe_steps.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS recipe_steps (
-  id serial int NOT NULL,
-  step_no int NOT NULL,
-  PRIMARY KEY (id, step_no),
-  ingredient_unit TEXT,
-  ingredient_quantity TEXT,
-  description TEXT NOT NULL,
-  CHECK (description <> '')
-);
