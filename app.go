0e9c83a60f60af307072e2487dfca195a12858e6
Jon Ho <jonwho.contact@gmail.com>
2018-05-23 23:41:52 -0700

    move resource routes out of app

--- app.go
+++ app.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -33,25 +31,17 @@ func (a *App) Initialize(user, password, dbname string) {
 
 	a.Router = mux.NewRouter()
 
-	// ping
-	a.Router.HandleFunc("/ping", a.pingHandler)
+	ping_router := PingRouter{}
+	ping_router.Route(a.Router)
 
-	// users
-	a.Router.HandleFunc("/users", a.ListUsers).Methods("GET")
-	a.Router.HandleFunc("/users", a.CreateUser).Methods("POST")
-	a.Router.HandleFunc("/users/{id:[0-9]+}", a.GetUser).Methods("GET")
-	a.Router.HandleFunc("/users/{id:[0-9]+}", a.UpdateUser).Methods("PUT")
-	a.Router.HandleFunc("/users/{id:[0-9]+}", a.DeleteUser).Methods("DELETE")
+	users_router := UsersRouter{DB: a.DB}
+	users_router.Route(a.Router)
 
-	// recipes
-	a.Router.HandleFunc("/recipes", a.ListRecipes).Methods("GET")
-	a.Router.HandleFunc("/recipes", a.CreateRecipe).Methods("POST")
-	a.Router.HandleFunc("/recipes/{id:[0-9]+}", a.GetRecipe).Methods("GET")
-	a.Router.HandleFunc("/recipes/{id:[0-9]+}", a.UpdateRecipe).Methods("PUT")
-	a.Router.HandleFunc("/recipes/{id:[0-9]+}", a.DeleteRecipe).Methods("DELETE")
+	recipes_router := RecipesRouter{DB: a.DB}
+	recipes_router.Route(a.Router)
 
 	// protected endpoint requiring jwt auth
-	a.Router.HandleFunc("/private", a.privateResource).Methods("GET", "POST", "PUT", "PATCH", "DELETE")
+	a.Router.HandleFunc("/private", a.PrivateResource).Methods("GET", "POST", "PUT", "PATCH", "DELETE")
 
 	a.Router.Use(loggingMiddleware)
 
@@ -60,7 +50,7 @@ func (a *App) Initialize(user, password, dbname string) {
 
 // Start the app
 func (a *App) Run(addr string) {
-	log.Println("Starting api... listening on port", addr)
+	log.Println("Starting api... Listening on port", addr)
 	log.Fatal(http.ListenAndServe(addr, a.Router))
 }
 
@@ -69,209 +59,7 @@ func (a *App) pingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong!")
 }
 
-func (a *App) ListRecipes(w http.ResponseWriter, r *http.Request) {
-	// limit to 10 recipes for now
-	recipes, err := getRecipes(a.DB, 0, 10)
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-	}
-
-	respondWithJSON(w, http.StatusOK, recipes)
-}
-
-func (a *App) ListUsers(w http.ResponseWriter, r *http.Request) {
-	// limit to 10 users for now, probably remove limit restriction later
-	users, err := getUsers(a.DB, 0, 10)
-
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-	}
-
-	respondWithJSON(w, http.StatusOK, users)
-}
-
-func (a *App) CreateRecipe(w http.ResponseWriter, r *http.Request) {
-	var rec recipe
-	decoder := json.NewDecoder(r.Body)
-
-	if err := decoder.Decode(&rec); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	defer r.Body.Close()
-
-	if err := rec.createRecipe(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, rec)
-}
-
-func (a *App) CreateUser(w http.ResponseWriter, r *http.Request) {
-	var u user
-	decoder := json.NewDecoder(r.Body)
-
-	if err := decoder.Decode(&u); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	defer r.Body.Close()
-
-	if err := u.createUser(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, u)
-}
-
-func (a *App) GetRecipe(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid recipe ID")
-		return
-	}
-
-	rec := recipe{ID: id}
-	if err := rec.getRecipe(a.DB); err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			respondWithError(w, http.StatusNotFound, "Recipe not found")
-		default:
-			respondWithError(w, http.StatusInternalServerError, err.Error())
-		}
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, rec)
-}
-
-func (a *App) GetUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
-		return
-	}
-
-	u := user{ID: id}
-	if err := u.getUser(a.DB); err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			respondWithError(w, http.StatusNotFound, "User not found")
-		default:
-			respondWithError(w, http.StatusInternalServerError, err.Error())
-		}
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, u)
-}
-
-func (a *App) UpdateRecipe(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid recipe ID")
-		return
-	}
-
-	var rec recipe
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&rec); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	defer r.Body.Close()
-	rec.ID = id
-
-	if err := rec.updateRecipe(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, rec)
-}
-
-func (a *App) UpdateUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
-		return
-	}
-
-	var u user
-	decoder := json.NewDecoder(r.Body)
-	// maps values from json that match back to struct
-	if err := decoder.Decode(&u); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	defer r.Body.Close()
-	u.ID = id
-
-	if err := u.updateUser(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, u)
-}
-
-func (a *App) DeleteRecipe(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid recipe ID")
-		return
-	}
-
-	rec := recipe{ID: id}
-	if err := rec.deleteRecipe(a.DB); err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, map[string]string{"data": "success"})
-}
-
-func (a *App) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
-		return
-	}
-
-	u := user{ID: id}
-	if err := u.deleteUser(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, map[string]string{"data": "success"})
-}
-
 // test auth in this route
-func (a *App) privateResource(w http.ResponseWriter, r *http.Request) {
+func (a *App) PrivateResource(w http.ResponseWriter, r *http.Request) {
 	return
-}
-
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, map[string]string{"error": message})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
 }
---
+++ recipes_router.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+type RecipesRouter struct {
+	Subrouter *mux.Router
+	DB        *sql.DB
+}
+
+func (rr *RecipesRouter) Route(router *mux.Router) {
+	rr.Subrouter = router.PathPrefix("/recipes").Subrouter()
+
+	rr.Subrouter.HandleFunc("", rr.ListRecipes).Methods("GET")
+	rr.Subrouter.HandleFunc("", rr.CreateRecipe).Methods("POST")
+	rr.Subrouter.HandleFunc("/{id:[0-9]+}", rr.GetRecipe).Methods("GET")
+	rr.Subrouter.HandleFunc("/{id:[0-9]+}", rr.UpdateRecipe).Methods("PUT")
+	rr.Subrouter.HandleFunc("/{id:[0-9]+}", rr.DeleteRecipe).Methods("DELETE")
+}
+
+func (rr *RecipesRouter) ListRecipes(w http.ResponseWriter, r *http.Request) {
+	// limit to 10 recipes for now
+	recipes, err := getRecipes(rr.DB, 0, 10)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	respondWithJSON(w, http.StatusOK, recipes)
+}
+
+func (rr *RecipesRouter) CreateRecipe(w http.ResponseWriter, r *http.Request) {
+	var rec recipe
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&rec); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	if err := rec.createRecipe(rr.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, rec)
+}
+
+func (rr *RecipesRouter) GetRecipe(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid recipe ID")
+		return
+	}
+
+	rec := recipe{ID: id}
+	if err := rec.getRecipe(rr.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Recipe not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, rec)
+}
+
+func (rr *RecipesRouter) UpdateRecipe(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid recipe ID")
+		return
+	}
+
+	var rec recipe
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&rec); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+	rec.ID = id
+
+	if err := rec.updateRecipe(rr.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, rec)
+}
+
+func (rr *RecipesRouter) DeleteRecipe(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid recipe ID")
+		return
+	}
+
+	rec := recipe{ID: id}
+	if err := rec.deleteRecipe(rr.DB); err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"data": "success"})
+}
