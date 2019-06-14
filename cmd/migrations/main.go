85693db1bbfccdaedb709e10e11704cc298c8579
Jon Ho <jonwho.contact@gmail.com>
2019-06-14 01:24:57 -0700

    add todo for cmd pkg

---
+++ cmd/migrations/main.go
@@ -0,0 +1,5 @@
+package main
+
+// TODO: impl migration command
+func main() {
+}
---
+++ cmd/server/main.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO: impl server command
+// 1. orchestrates all dependency injection here
+// 2. be sure to write subpackages to follow hexagon DI pattern
+// 3 subpackages should be able to work on DI args passed in and implement the interface how it sees fit
+func main() {
+}
